087c4bf5bac5dd67556deeb9aef26364f1db1cc7
konradcodes <konkordek47@gmail.com>
2019-03-30 23:34:04 +0000

    Markup and some styles finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
+//Global App Controller
+
+//Views
+
+//Models
+
 // CSS
 import "../css/main.scss";
+
+
+/** //? Global State of the app
+ * - Search Object
+ * - Current Recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
